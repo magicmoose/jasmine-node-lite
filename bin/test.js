b27e99a9619eb767716713bf04ee15050f4fc4d4
magicmoose <ralf@scardiecat.org>
2013-04-20 03:33:10 -0400

    add a coffee script sample

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,5 @@
 var jasminerunner = require('../lib/jasminerunner');
 
 
-jasminerunner.executeSpecs(['./spec/sample.spec.js']);
+jasminerunner.executeSpecs(['./spec/sample.spec.js',
+    './spec/literatecoffee.spec.litcoffee']);
